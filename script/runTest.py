bd7fb42e022748de42c8a92edbe52127f0e2db30
jtj <1553425481@qq.com>
2023-02-09 22:14:52 +0800

    "run Test demo"

--- script/runTest.py
+++ script/runTest.py
@@ -6,10 +6,10 @@
 import sys
 sys.path.append('/home/jiangtianjie/gdefects4dll')
 
-sys.path.insert(0,'../data')
+sys.path.insert(0, '../data')
 import argparse
 import os
-from pytorch_detail import get_dict
+from data.pytorch_detail import get_dict
 # python runTest.py torch-61345 buggy
 # docker exec -it pytorch-10.2-7 /bin/bash /home/torch-61345/torch-61345-buggy.sh
 # docker exec -it gdefects4dll-rebug" /bin/bash /test/torch-61345/torch-61345-buggy.sh
